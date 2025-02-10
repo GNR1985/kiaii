a6d6e981e6cac81c4e5935550ae873107fb15643
GNR1985 <slahspen0@gmail.com>
2025-02-10 15:04:22 -0300

    Add files via upload

--- open.js
+++ open.js
@@ -9,3 +9,23 @@ mobileMenu.addEventListener('click', () => {
 closeMenuBtn.addEventListener('click', () => {
     navList.classList.remove('open');
 });
+let button = document.getElementById("revel"); 
+let modal = document.querySelector(".audio"); 
+let closeModal1 = document.getElementById("close");
+document.querySelectorAll(".item-menu").forEach(item => {
+  const skate = item.querySelector(".skate");  
+
+  item.addEventListener("mouseenter", () => {
+    if (skate) {
+      skate.style.transition = "transform 0.5s ease-in-out";
+      skate.style.transform = "rotate(360deg)";
+    }
+  });
+
+  item.addEventListener("mouseleave", () => {
+    if (skate) {
+      skate.style.transition = "transform 0.5s ease-in-out";
+      skate.style.transform = "rotate(0deg)";
+    }
+  });
+});
